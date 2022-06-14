bfccfe9f4b3981d1bf06a907e5c2a5a7b4cd215f
lukmak0394 <lukmak0394@wp.pl>
2022-06-14 10:17:18 +0200

    changed toggle classes function

--- js/main.js
+++ js/main.js
@@ -4,20 +4,11 @@ const navBar = document.querySelector('.navbar');
 const mainNav = document.querySelector('.main-nav');
 
 const toggleClasses = () => {
-    if (navBar.classList.contains('navbar-opened')) {
-        navBar.classList.remove('navbar-opened');
-        navBar.classList.add('navbar-closed');
-        // Class of icon 
-        navBtn.children[0].classList.remove('fa-times');
-        navBtn.children[0].classList.add('fa-bars');
-        mainNav.classList.remove('navbar-bg-black');
-    } else {
-        navBar.classList.add('navbar-opened');
-        navBar.classList.remove('navbar-closed');
-        navBtn.children[0].classList.add('fa-times');
-        navBtn.children[0].classList.remove('fa-bars');
-        mainNav.classList.add('navbar-bg-black');
-    }
+
+    navBar.classList.toggle('navbar-opened');
+    mainNav.classList.toggle('navbar-bg-black');
+    navBtn.children[0].classList.toggle('fa-times')
+
 };
 
 navBtn.addEventListener('click',toggleClasses);
